18ed2f67545c08a25292ac13ef92340768291174
Sandipan Dey <hey@sandipan.dev>
2021-07-26 20:21:12 +0530

    added restoring for master xprv

--- src/lib.rs
+++ src/lib.rs
@@ -5,5 +5,8 @@ mod routes;
 
 #[launch]
 pub fn server() -> _ {
-    rocket::build().mount("/key", routes![routes::keys::key_generate])
+    rocket::build().mount(
+        "/key",
+        routes![routes::keys::key_generate, routes::keys::key_restore],
+    )
 }
--- src/routes/keys.rs
+++ src/routes/keys.rs
@@ -14,18 +14,36 @@ pub struct MasterKey {
 }
 
 #[derive(Debug, Deserialize, Default)]
-pub struct MasterKeyOpts {
+pub struct MasterKeyGenerateOpts {
     word_count: Option<u16>,
     password: Option<String>,
 }
 
 #[post("/generate", format = "json", data = "<opts>")]
-pub fn key_generate(opts: Json<MasterKeyOpts>) -> Json<MasterKey> {
+pub fn key_generate(opts: Json<MasterKeyGenerateOpts>) -> Json<MasterKey> {
     let mnemonic_type = match opts.word_count {
         Some(12) => MnemonicType::Words12,
         _ => MnemonicType::Words24,
     };
     let mnemonic = Mnemonic::new(mnemonic_type, Language::English);
+    let xprv = bip32::ExtendedPrivKey::new_master(Network::Bitcoin, mnemonic.entropy()).unwrap();
+    let secp = Secp256k1::new();
+    Json(MasterKey {
+        mnemonic: mnemonic.phrase().to_string(),
+        master_xprv: xprv.to_string(),
+        fingerprint: xprv.fingerprint(&secp).to_string(),
+    })
+}
+
+#[derive(Debug, Deserialize, Default)]
+pub struct MasterKeyRestoreOpts {
+    mnemonic: String,
+    password: Option<String>,
+}
+
+#[post("/restore", format = "json", data = "<opts>")]
+pub fn key_restore(opts: Json<MasterKeyRestoreOpts>) -> Json<MasterKey> {
+    let mnemonic = Mnemonic::from_phrase(&opts.mnemonic, Language::English).unwrap();
     let xprv = bip32::ExtendedPrivKey::new_master(Network::Bitcoin, mnemonic.entropy()).unwrap();
     let secp = Secp256k1::new();
     Json(MasterKey {
